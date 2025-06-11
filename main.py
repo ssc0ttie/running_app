26e5c63254ed0d42fe6204f0ffff3950db3297e0
ssc0ttie <ksmacawile@gmail.com>
2025-06-11 17:36:21 +0800

    layout updates plus stats

--- main.py
+++ main.py
@@ -16,163 +16,193 @@ Welcome_msg = (
 st.subheader(Welcome_msg)
 
 # # LAYOUT COLOUMNS
-# col1, col2 = st.columns(2)
+tab1, tab2, tab3 = st.tabs(["Training Log", "Program", "Stats"])
 
-# with col1:
+with tab1:
 
-with st.form("activity_log", clear_on_submit=True, border=True):
-    time_stamp_ = datetime.now()
-    time_stamp = time_stamp_.strftime("%Y-%m-%d")
-    mem_selection = st.multiselect(
-        "Members",
-        ["Aiza", "Chona", "Fraulein", "Lead", "Maxine", "Scott"],
-        max_selections=1,
-    )
-    # member_name = st.markdown(f"Select Member", {mem_selection})
-    date = st.date_input("Date of Activity", value=datetime.today())
+    with st.form("activity_log", clear_on_submit=True, border=True):
+        time_stamp_ = datetime.now()
+        time_stamp = time_stamp_.strftime("%Y-%m-%d")
+        mem_selection = st.multiselect(
+            "Members",
+            ["Aiza", "Chona", "Fraulein", "Lead", "Maxine", "Scott"],
+            max_selections=1,
+        )
+        # member_name = st.markdown(f"Select Member", {mem_selection})
+        date = st.date_input("Date of Activity", value=datetime.today())
 
-    # Activity list
-    act_selection = st.multiselect(
-        "Activity",
-        [
-            "5k LSD Road@ Zone 2 Pace",
-            "6k LSD Road@ Zone 2 Pace",
-            "8k LSD Road@ Zone 2 Pace",
-            "10k LSD Road@ Zone 2 Pace",
-            "10k LSD Trail@ Zone 2 Pace",
-            "11k LSD Road@ Zone 2 Pace",
-            "12k LSD Road@ Zone 2 Pace",
-            "14k LSD Road@ Zone 2 Pace",
-            "15k LSD Road@ Zone 2 Pace",
-            "15k LSD Trail@ Zone 2 Pace",
-            "17k LSD Road@ Zone 2 Pace",
-            "18k LSD Road@ Zone 2 Pace",
-            "19k LSD Road@ Zone 2 Pace",
-            "20k LSD Road@ Zone 2 Pace",
-            "20k LSD Trail@ Zone 2 Pace",
-            "21k LSD Trail@ Zone 2 Pace",
-            "24k LSD Trail@ Zone 2 Pace",
-            "26k LSD Road@ Zone 2 Pace",
-            "27k LSD Trail@ Zone 2 Pace",
-            "28k LSD Road@ Zone 2 Pace",
-            "31k LSD Road@ Zone 2 Pace",
-            "20 Mins Easy Run",
-            "30 Mins Easy Run",
-            "35 Mins Easy Run",
-            "40 Mins Easy Run",
-            "45 Mins Easy Run",
-            "50 Mins Easy Run",
-            "55 Mins Easy Run",
-            "60 Mins Easy Run",
-            "RACE DAY",
-            "Rest",
-            "Speed Work (Zone 4 : 3x400M)",
-            "Speed Work (Zone 4 : 4x400M)",
-            "Speed Work (Zone 4 : 5x400M)",
-            "Speed Work (Zone 4 : 6x400M)",
-            "Strength Training",
-            "Cross Train",
-            "1k Tempo",
-            "2k Tempo",
-            "3k Tempo",
-            "4k Tempo",
-            "5k Tempo",
-            "6k Tempo",
-            "7k Tempo",
-            "8k Tempo",
-            "9k Tempo",
-            "10k Tempo",
-        ],
-        max_selections=1,
-    )
+        # Activity list
+        act_selection = st.multiselect(
+            "Activity",
+            [
+                "5k LSD Road@ Zone 2 Pace",
+                "6k LSD Road@ Zone 2 Pace",
+                "8k LSD Road@ Zone 2 Pace",
+                "10k LSD Road@ Zone 2 Pace",
+                "10k LSD Trail@ Zone 2 Pace",
+                "11k LSD Road@ Zone 2 Pace",
+                "12k LSD Road@ Zone 2 Pace",
+                "14k LSD Road@ Zone 2 Pace",
+                "15k LSD Road@ Zone 2 Pace",
+                "15k LSD Trail@ Zone 2 Pace",
+                "17k LSD Road@ Zone 2 Pace",
+                "18k LSD Road@ Zone 2 Pace",
+                "19k LSD Road@ Zone 2 Pace",
+                "20k LSD Road@ Zone 2 Pace",
+                "20k LSD Trail@ Zone 2 Pace",
+                "21k LSD Trail@ Zone 2 Pace",
+                "24k LSD Trail@ Zone 2 Pace",
+                "26k LSD Road@ Zone 2 Pace",
+                "27k LSD Trail@ Zone 2 Pace",
+                "28k LSD Road@ Zone 2 Pace",
+                "31k LSD Road@ Zone 2 Pace",
+                "20 Mins Easy Run",
+                "30 Mins Easy Run",
+                "35 Mins Easy Run",
+                "40 Mins Easy Run",
+                "45 Mins Easy Run",
+                "50 Mins Easy Run",
+                "55 Mins Easy Run",
+                "60 Mins Easy Run",
+                "RACE DAY",
+                "Rest",
+                "Speed Work (Zone 4 : 3x400M)",
+                "Speed Work (Zone 4 : 4x400M)",
+                "Speed Work (Zone 4 : 5x400M)",
+                "Speed Work (Zone 4 : 6x400M)",
+                "Strength Training",
+                "Cross Train",
+                "1k Tempo",
+                "2k Tempo",
+                "3k Tempo",
+                "4k Tempo",
+                "5k Tempo",
+                "6k Tempo",
+                "7k Tempo",
+                "8k Tempo",
+                "9k Tempo",
+                "10k Tempo",
+            ],
+            max_selections=1,
+        )
 
-    distance = st.number_input("Distance")
-    pace = st.time_input("Pace (min/km)", value=time(8, 45), step=60)
-    hr = st.number_input("HR (bmp)", min_value=0, max_value=220)
-    cad = st.number_input("Cadence (spm)", min_value=0, max_value=200)
-    rpe = st.slider("RPE", 0, 10, 1)
-    rpe2 = st.feedback(options="faces", key=int)
-    shoe = st.multiselect(
-        "Shoe",
-        [
-            "Adidas Adizero SL2",
-            "Asics Purple",
-            "Boston 12",
-            "NB Rebel v3",
-            "Nike",
-            "Nike Winflo 10",
-            "Novablast 3",
-            "Adidas Pink",
-            "Reebok Float Ride Energy 4",
-            "Sketcher",
-        ],
-        max_selections=1,
-    )
-    remarks = st.text_area("Remarks")
+        distance = st.number_input("Distance")
+        pace = st.time_input("Pace (min/km)", value=time(8, 45), step=60)
+        hr = st.number_input("HR (bmp)", min_value=0, max_value=220)
+        cad = st.number_input("Cadence (spm)", min_value=0, max_value=200)
+        rpe = st.slider("RPE", 0, 10, 1)
+        rpe2 = st.feedback(options="faces", key=int)
+        shoe = st.multiselect(
+            "Shoe",
+            [
+                "Adidas Adizero SL2",
+                "Asics Purple",
+                "Boston 12",
+                "NB Rebel v3",
+                "Nike",
+                "Nike Winflo 10",
+                "Novablast 3",
+                "Adidas Pink",
+                "Reebok Float Ride Energy 4",
+                "Sketcher",
+            ],
+            max_selections=1,
+        )
+        remarks = st.text_area("Remarks")
 
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Submit Log")
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Submit Log")
 
-    #############PUSH DATA####################
+        #############PUSH DATA####################
 
-    if submitted:
+        if submitted:
 
-        new_log = [
-            time_stamp,  # Convert datetime to ISO string
-            date.isoformat(),  # Convert date to ISO string
-            (
-                act_selection[0] if act_selection else ""
-            ),  # Get first selected activity or empty
-            distance,
-            pace.strftime("0:%H:%M"),  # Convert time to string (e.g., "08:45")
-            hr,
-            cad,
-            rpe,
-            shoe[0] if shoe else "",  # Get first selected shoe or empty
-            remarks,
-            (
-                mem_selection[0] if mem_selection else ""
-            ),  # Get first selected member or empty
-        ]
+            new_log = [
+                time_stamp,  # Convert datetime to ISO string
+                date.isoformat(),  # Convert date to ISO string
+                (
+                    act_selection[0] if act_selection else ""
+                ),  # Get first selected activity or empty
+                distance,
+                pace.strftime("0:%H:%M"),  # Convert time to string (e.g., "08:45")
+                hr,
+                cad,
+                rpe,
+                shoe[0] if shoe else "",  # Get first selected shoe or empty
+                remarks,
+                (
+                    mem_selection[0] if mem_selection else ""
+                ),  # Get first selected member or empty
+            ]
 
-        push.push_runner_data(new_log)
-        ###Submit Notice
-        with st.spinner("Wait lang...", show_time=True):
-            tm.sleep(2)
-        st.success("Done! Activity Recorded!")
-        st.balloons()
+            push.push_runner_data(new_log)
+            ###Submit Notice
+            with st.spinner("Wait lang...", show_time=True):
+                tm.sleep(2)
+            st.success("Done! Activity Recorded!")
+            st.balloons()
 
 ###############TRAINING PLAN SECTION#############################################
-st.subheader("TRAINING PLAN", divider="blue")
+with tab2:
+    st.header("TRAINING PLAN", divider="blue")
 
-with st.expander("View Training Program"):
+    # with st.expander("View Training Program"):
     prog_sheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRF_uf-orH_71Ibql9N1QZ2FSWblHhvX2_KzjN_SLOSlchsDz0Mo8jOBI9mQOONyeKJR4pEQOjXAjKt/pubhtml?gid=0&single=true"
     components.iframe(
         prog_sheet,
         width=1500,
         height=800,
     )
+######METRICS########
+with tab3:
+    st.header("                STATS                ", divider="blue")
+    import numpy as np
 
-#####################################TEST CHART#####################
+    df = pd.DataFrame(pull.get_runner_data())
 
-df = pd.DataFrame(pull.get_runner_data())
-# ensure that datetime is a date
-# df["Date_of_Activity"] = pd.to_datetime(df["Date_of_Activity"])
+    metric_distance = int(df["Distance"].sum())
 
-# filters all distance > zero
-df = df[df["Distance"] > 0 & df["Distance"].notna()]
+    # TOTAL MOVING TIME
+    total_seconds = int(df["Moving_Time"].sum().total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    metric_movingtime = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
-##GROUP BY
-act_date_group = df.groupby("Week", as_index=False)
+    # AVG PACE
+    avg_pace = pd.to_timedelta(df["Pace"]).mean()
+    metric_pace = f"{int(avg_pace.total_seconds() // 60):02d}:{int(avg_pace.total_seconds() % 60):02d}"
 
-st.subheader("Distance")
+    # .sum() / df["Pace"].len()
 
-line_data = act_date_group["Distance"].sum()
+    col1, col2, col3 = st.columns(3)
+    col1.metric(
+        "Total Distance in kms",
+        value=metric_distance,
+        label_visibility="visible",
+        border=True,
+    )
+    col2.metric(
+        "Moving Time", value=metric_movingtime, label_visibility="visible", border=True
+    )
+    col3.metric(
+        "Average Pace", value=metric_pace, label_visibility="visible", border=True
+    )
 
-st.bar_chart(line_data, x="Week", y="Distance", y_label="Distance")
+    #####################################TEST CHART#####################
 
+    st.subheader("Distance")
+    # filters all distance > zero
+    df = df[df["Distance"] > 0 & df["Distance"].notna()]
 
-# ##FILTER BY MEMBER ##
-# filt = df["Member_Name"] == mem_selection
+    ##GROUP BY
+    act_date_group = df.groupby("Week", as_index=False)
 
-# df.loc[filt]
+    line_data = act_date_group["Distance"].sum()
+
+    st.bar_chart(line_data, x="Week", y="Distance", y_label="Distance")
+
+    # ##FILTER BY MEMBER ##
+    # filt = df["Member_Name"] == mem_selection
+
+    # df.loc[filt]
