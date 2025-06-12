6558edb85fb94ac4f0f59a8d86b7723ffcee22c8
ssc0ttie <ksmacawile@gmail.com>
2025-06-12 17:24:39 +0800

    revert combo

--- visuals/combochart.py
+++ visuals/combochart.py
@@ -24,41 +24,33 @@ def generate_combo(data):
     # Add bar chart for Distance
     fig.add_trace(
         go.Bar(
-            y=dist_data["Week"],
-            x=dist_data["Distance"],
-            orientation="h",
+            x=dist_data["Week"],
+            y=dist_data["Distance"],
             name="Distance (km)",
             text=dist_data["Distance"],
             marker=dict(color="mediumaquamarine"),
-        ),
-        secondary_y=False,
+        )
     )
 
     # Add line chart for Pace
     fig.add_trace(
         go.Scatter(
-            y=dist_data["Week"],
-            x=pace_data["Pace_Mins"],
+            x=dist_data["Week"],
+            y=pace_data["Pace_Mins"],
             name="Pace (min/km)",
-            orientation="h",
-            mode="lines+markers+text",
+            yaxis="y2",
             text=pace_data["Pace_Str"],
             marker=dict(size=12, line=dict(width=2, color="crimson")),
         ),
-        secondary_y=True,
     )
 
     # Add second y-axis for Pace
     fig.update_layout(title="Distance x Pace")
-    # fig.update_yaxes(title_text="Week", categoryorder ="category ascending", secondary_y=False)
-    # fig.update_xaxes(title_text="Distance (km)")
-    # fig.update_xaxes(title_text="Pace (min/km)", secondary_x=True)
-
-    fig.update_xaxes(title_text="Distance (km) / Pace (min/km)", range=[0, 15])
-    fig.update_yaxes(
-        title_text="Week", categoryorder="category ascending", secondary_y=False
+    fig.update_xaxes(title_text="Week")
+    fig.update_yaxes(title_text="KMS", secondary_y=False)
+    fig.update_yaxes(title_text="Pace (min/km)", secondary_y=True)
+    fig.update_layout(
+        width=800,  # Set the desired width in pixels
+        height=300,  # Set the desired height in pixels
     )
-    fig.update_yaxes(showgrid=False, secondary_y=True)
-    fig.update_layout(width=500, height=800)
-
     st.plotly_chart(fig, use_container_width=True, key="combo_chart")
