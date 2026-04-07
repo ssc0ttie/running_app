722fea05c3009fc1bd4452ee2447a960a94facd3
ssc0ttie <ksmacawile@gmail.com>
2026-04-07 11:14:01 +0800

    data source update

--- main.py
+++ main.py
@@ -205,19 +205,20 @@ st.text("")
 
 ##########################################################################
 
+if tabs != "🗓️ Program":
 
-if st.session_state.get("just_submitted", False):
-    df = pulluc.get_runner_data()  # Uncached for fresh data after submission
-    st.session_state["just_submitted"] = False
-else:
-    df = pullc.get_runner_data()  # Cached for normal viewing
-    if df is None:
-        # Fallback to empty dataframe or previous data
-        df = pd.DataFrame()
+    if st.session_state.get("just_submitted", False):
+        df = pulluc.get_runner_data()  # Uncached for fresh data after submission
+        st.session_state["just_submitted"] = False
+    else:
+        df = pullc.get_runner_data()  # Cached for normal viewing
+        if df is None:
+            # Fallback to empty dataframe or previous data
+            df = pd.DataFrame()
 
-#############################################################################
+    #############################################################################
 
-full_df = pd.DataFrame(df)
+    full_df = pd.DataFrame(df)
 
 #################################----LOG TAB ------ ################################
 
